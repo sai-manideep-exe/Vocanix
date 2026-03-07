04eeb488e9d70d394eaf9503c1d27cf0f5b97e78
Sai Manideep Nagulapati <122035805+sai-manideep-exe@users.noreply.github.com>
2026-03-07 19:57:39 +0530

    Update model name in generateText function

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
 
     try {
         const { text: questions } = await generateText({
-            model: google('-2.5-flash'),
+            model: google('gemini-2.5-flash'),
             prompt: `Prepare questions for a job interview.
                     The job role is ${role}.
                     The job experience level is ${level}.
